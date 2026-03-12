e14e5a70e5f21d7d38f616ca2c9878201cc53ead
David Umoru <umorudavido@gmail.com>
2026-03-12 18:50:17 +0100

    delete existing codebase

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import DitherGenerator from "@/components/dither-generator"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background">
-      <DitherGenerator />
-    </main>
-  )
-}
